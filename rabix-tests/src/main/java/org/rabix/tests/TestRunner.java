054360fd31466a7aef0b839dfaeb91e606a2fea2
markosbg <marko.vukobrat@sbgenomics.com>
2016-06-13 18:59:18 +0200

    upd runner

--- rabix-tests/src/main/java/org/rabix/tests/TestRunner.java
+++ rabix-tests/src/main/java/org/rabix/tests/TestRunner.java
@@ -41,7 +41,10 @@ public class TestRunner {
     
     if (dir.isDirectory()) {
       if (directoryListing != null) {
-        executeCommand("tar -zxvf rabix-backend-local-0.0.1-SNAPSHOT-id3.tar.gz");
+        //executeCommand("tar -zxvf rabix-backend-local-0.0.1-SNAPSHOT-id3.tar.gz"); // for local execution
+        executeCommand("tar -zxvf rabix-backend-local/target/rabix-backend-local-0.0.1-SNAPSHOT-id3.tar.gz"); // for travis execution
+        
+        
          // TODO for inner testing - delete later
          System.out.println("***** absolute path testing: *****");
          executeCommand("./rabix.sh ../../rabix-tests/testbacklog/grep.cwl.yaml ../../rabix-tests/testbacklog/grep.inputs.yaml > result.yaml");
